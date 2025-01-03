567320236a2bf973df6214737e87f1a7548895b9
Azhar Mahmood <mywritingfrenzy@gmail.com>
2025-01-03 21:31:19 +0530

    Feature - / route comments

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,13 @@ export function createServer() {
 		})
 	);
 
+	// main route commenting
+	app.get('/', (req: Request, res: Response, next: NextFunction) => {
+		res.send(
+			'Welcome to the music library API \n Please use https://music-api.hitheredevs.com/api/v1/{your-desired-route} to access the API.'
+		);
+	});
+
 	// Body parser
 	app.use(express.json());
 
